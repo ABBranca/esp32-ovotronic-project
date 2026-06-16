9168d5ba3599f79341f966d1e4d30a2057507580
Alessandro Biagio Branca <335244@studenti.unimore.it>
2026-06-16 23:50:17 +0200

    Add LCD1602 driver functions for sending bytes and initializing the display

--- components/lcd1602/include/lcd1602.h
+++ components/lcd1602/include/lcd1602.h
@@ -4,6 +4,10 @@
 #include "esp_err.h"
 
 #define LCD1602_ADDRESS 0x27
+#define LCD_RS 0x01 // P0
+#define LCD_RW 0x02 // P1 (sempre 0)
+#define LCD_EN 0x04 // P2
+#define LCD_BL 0x08 // P3 Backlight
 
 extern i2c_device_config_t lcd1602_cfg;
 extern i2c_master_dev_handle_t lcd1602_handle;
--- components/lcd1602/lcd1602.c
+++ components/lcd1602/lcd1602.c
@@ -5,6 +5,7 @@
 #include "freertos/idf_additions.h"
 #include "hal/i2c_types.h"
 #include "portmacro.h"
+#include <stdint.h>
 
 i2c_device_config_t lcd1602_cfg = {
     .dev_addr_length = I2C_ADDR_BIT_LEN_7,
@@ -14,12 +15,61 @@ i2c_device_config_t lcd1602_cfg = {
 
 i2c_master_dev_handle_t lcd1602_handle;
 
+static uint8_t lcd_backlight = LCD_BL;
+
+static esp_err_t lcd_pulse_enable(uint8_t data) {
+
+  uint8_t b = data | LCD_EN;
+  esp_err_t err = i2c_master_transmit(lcd1602_handle, &b, 1, -1);
+  if (err != ESP_OK)
+    return err;
+  b = data & ~LCD_EN;
+  err = i2c_master_transmit(lcd1602_handle, &b, 1, -1);
+  if (err != ESP_OK)
+    return err;
+
+  return ESP_OK;
+}
+
+static esp_err_t lcd_send_nibble(uint8_t nibble, uint8_t mode) {
+  uint8_t data = (nibble << 4) | mode | lcd_backlight;
+  return lcd_pulse_enable(data);
+}
+
+static esp_err_t lcd_send_byte(const uint8_t byte, const uint8_t mode) {
+  uint8_t alto = byte >> 4;
+  esp_err_t err = lcd_send_nibble(alto, mode);
+  if (err != ESP_OK)
+    return err;
+
+  uint8_t basso = byte & 0x0F;
+  err = lcd_send_nibble(basso, mode);
+  if (err != ESP_OK)
+    return err;
+
+  return ESP_OK;
+}
+
 // LCD1602 init Function API
 esp_err_t lcd1602_init(i2c_master_bus_handle_t bus) {
   ESP_ERROR_CHECK(
       i2c_master_bus_add_device(bus, &lcd1602_cfg, &lcd1602_handle));
 
   vTaskDelay(40 / portTICK_PERIOD_MS);
+
+  for (uint8_t i = 0; i < 3; i++) {
+    lcd_send_nibble(0x3, 0);
+    vTaskDelay(40 / portTICK_PERIOD_MS);
+  }
+
+  lcd_send_nibble(0x2, 0);
+
+  lcd_send_byte(0x28, 0);
+  lcd_send_byte(0x08, 0);
+  lcd_send_byte(0x01, 0);
+  vTaskDelay(20 / portTICK_PERIOD_MS);
+  lcd_send_byte(0x06, 0);
+  lcd_send_byte(0x0c, 0);
 
   return ESP_OK;
 }
